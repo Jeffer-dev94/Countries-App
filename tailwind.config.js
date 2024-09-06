a1b1c28647f82eecb4050396f3f8607db149a477
Jefferson Pérez <jeffersonperez1608@gmail.com>
2024-09-05 22:52:25 -0500

    fix: add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./src/**/*.{vue,js,ts}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      'xxs': '375px',
+      'xs': '420px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        app: {
+          blue: {
+            700: "#2b3a4d",
+            800: "#1e2a39",
+            900: "#002b49",
+          },
+          gray: {
+            300: "#fafafa",
+            900: "#868686"
+          },
+        },
+      },
+      fontFamily: {
+        'nunito': ['"Nunito Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
-
+};
