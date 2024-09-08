e5f5a1c1f5f87c3a4664bfc2076321a0c4ac94fd
Jefferson Pérez <jeffersonperez1608@gmail.com>
2024-09-08 14:44:39 -0500

    fix: add country card

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-white dark:bg-app-blue-700 grid rounded-lg overflow-hidden shadow">
+    <figure class="w-full h-auto aspect-[264/160] flex">
+      <img :src="flags.png" alt="" class="size-full object-cover object-center" />
+    </figure>
+
+    <div class="px-8 pt-9 pb-14 text-app-blue-900 dark:text-white">
+      <h2 class="text-xl font-bold mb-5">{{ name.common }}</h2>
+      <p class="py-1"><strong>Population:</strong> {{ (population.toLocaleString('es-CO')) }}</p>
+      <p class="py-1"><strong>Region:</strong> {{ region }}</p>
+      <p v-if="capital" class="py-1"><strong>Capital:</strong> {{ capital?.join(',') }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  name: {
+    type: Object,
+    required: true
+  },
+  population: {
+    type: Number,
+    required: true
+  },
+  region: {
+    type: String,
+    required: true
+  },
+  capital: {
+    type: Array
+  },
+  flags: {
+    type: Object,
+    required: true
+  }
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CountryCard from '@/components/CountryCard.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import SelectInput from '@/components/SelectInput.vue';
 
@@ -7,11 +8,10 @@ import { storeToRefs } from 'pinia';
 
 const store = useCountriesStore();
 const { countriesList, regionList } = storeToRefs(store);
-console.log(countriesList);
 </script>
 
 <template>
-  <section class="flex flex-col py-[30px]">
+  <section class="flex flex-col gap-10 md:gap-12 py-[30px]">
     <div class="flex flex-col md:flex-row gap-[50px] md:gap-6 justify-between">
       <div class="w-full md:w-3/5 lg:w-[480px]">
         <SearchInput />
@@ -19,6 +19,10 @@ console.log(countriesList);
       <div class="w-full sm:w-3/5 md:w-52 md:max-w-[40%]">
         <SelectInput :items="regionList" placeholder="Filter by Region" />
       </div>
+    </div>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-12 lg:gap-[72px]">
+      <CountryCard v-for="item in countriesList" :key="item.name.common" :name="item.name" :population="item.population"
+        :region="item.region" :capital="item.capital" :flags="item.flags" />
     </div>
   </section>
 </template>
