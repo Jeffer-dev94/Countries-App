cf7c5ddca32c5bfce093abb186b7b7bd5e9485bf
Jefferson Pérez <jeffersonperez1608@gmail.com>
2024-10-03 23:44:14 -0500

    fix: add skeleton for details

--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -64,6 +64,42 @@
         </div>
       </div>
     </div>
+    <div
+      v-else-if="!countryData && isFetching"
+      class="flex flex-nowrap flex-col md:flex-row gap-20 md:gap-16 xl:gap-[120px] animate-pulse"
+    >
+      <div class="w-full lg:w-1/2 lg:max-w-[560px]">
+        <div class="w-full aspect-[560/400] bg-gray-200 dark:bg-slate-600 rounded"></div>
+      </div>
+
+      <div class="w-full lg:w-[calc(50%+40px)] lg:grow">
+        <div class="h-9 rounded w-full bg-gray-200 dark:bg-slate-600 mb-9"></div>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-5 mb-10">
+          <ul class="flex flex-col gap-1">
+            <li class="h-5 rounded w-full bg-gray-200 dark:bg-slate-600"></li>
+            <li class="h-5 rounded w-5/6 bg-gray-200 dark:bg-slate-600"></li>
+            <li class="h-5 rounded w-4/6 bg-gray-200 dark:bg-slate-600"></li>
+            <li class="h-5 rounded w-5/6 bg-gray-200 dark:bg-slate-600"></li>
+            <li class="h-5 rounded w-3/6 bg-gray-200 dark:bg-slate-600"></li>
+          </ul>
+          <ul class="flex flex-col gap-2">
+            <li class="h-5 rounded w-4/6 bg-gray-200 dark:bg-slate-600"></li>
+            <li class="h-5 rounded w-5/6 bg-gray-200 dark:bg-slate-600"></li>
+            <li class="h-5 rounded w-3/6 bg-gray-200 dark:bg-slate-600"></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div
+      v-else
+      class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+      role="alert"
+    >
+      <span class="block font-bold pr-2">Oops!</span>
+      <span class="sm:inline"
+        >No data available for the country <strong>{{ countryName }}</strong></span
+      >
+    </div>
   </section>
 </template>
 
@@ -79,7 +115,7 @@ const route = useRoute();
 const countryName = ref(route.params.name);
 
 const store = useCountriesStore();
-const { countriesData } = storeToRefs(store);
+const { countriesData, isFetching } = storeToRefs(store);
 
 const countryData = ref(null);
 const nativeName = ref(null);
@@ -121,7 +157,14 @@ watch(
   }
 );
 
+watch(
+  () => countriesData.value,
+  () => {
+    setCountry();
+  }
+);
+
 onMounted(() => {
-  setCountry();
+  if (countriesData.value.length) setCountry();
 });
 </script>
